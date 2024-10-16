a37780f1e6e3d9035681bd2dfad709f8caba1dc5
Yiğithan Gümüş <yigithangumuss@gmail.com>
2024-10-16 17:22:58 +0300

    Create categoryController.js

---
+++ controllers/categoryController.js
@@ -0,0 +1,58 @@
+const Category = require("../models/Category");
+
+const createCategory = async (req, res) => {
+    const newCategory = new Category(req.body);
+    try {
+        const savedCategory = await newCategory.save();
+        res.status(200).json(savedCategory);
+    } catch (err) {
+        res.status(500).json("Hata oluştu."+err);
+    }
+}
+
+const updateCategory = async (req, res) => {
+    try {
+        const updatedCategory = await Category.findByIdAndUpdate(req.params.categoryID, { $set: req.body }, { new: true });
+        res.status(200).json(updatedCategory);
+    } catch (err) {
+        res.status(500).json("Hata oluştu."+err);
+    }
+}
+
+const deleteCategory = async (req, res) => {
+    try {
+        await Category.findByIdAndDelete(req.params.categoryID);
+        res.status(200).json("Başarıyla kategori silindi.");
+    } catch (err) {
+        res.status(500).json("Hata oluştu."+err);
+    }
+}
+
+const getCategory = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.categoryID);
+        res.status(200).json(category);
+    } catch (err) {
+        res.status(500).json("Hata oluştu."+err);
+    }
+}
+
+const getAllCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).json(categories);
+    } catch (err) {
+        res.status(500).json("Hata oluştu."+err);
+    }
+}
+
+
+
+
+module.exports = {
+    createCategory,
+    updateCategory,
+    deleteCategory,
+    getCategory,
+    getAllCategories,
+};
