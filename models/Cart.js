5a7c346f9f0450bbb39ff9195041bbe341e72140
Yiğithan Gümüş <yigithangumuss@gmail.com>
2024-10-18 15:10:48 +0300

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     products: [{
       product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
       quantity: { type: Number, required: true }
     }],
-    totalPrice: { type: Number, required: true },
+    totalPrice: { type: Number },
     updatedAt: { type: Date, default: Date.now }
   }, {timestamps: true});
 
