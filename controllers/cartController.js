d70d0f91a33c8fcf14ee9fec22d4b3848eccb082
Yiğithan Gümüş <yigithangumuss@gmail.com>
2024-10-18 15:10:45 +0300

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,25 +1,40 @@
 const Product = require("../models/Product");
 const Cart = require("../models/Cart");
+const { getInformation } = require("../utils/functions");
 
 const createCart = async (req, res) => {
-
-       
     const user = await getInformation(req, res);
-    res.status(200).json(user);
+    const productData = await Product.findById(req.body.products[0].product);
 
-    // const newCart = new Cart(req.body);
-    // try {
-    //     const savedCart = await newCart.save();
-    //     res.status(200).json(savedCart);
-    // } catch (err) {
-    //     res.status(500).json("Hata oluştu." + err);
-    // }
-}
 
-const updateCart = async (req, res) => {
     try {
-        const updatedCart = await Cart.findByIdAndUpdate(req.params.cartID, { $set: req.body }, { new: true });
-        res.status(200).json(updatedCart);
+   
+        let carts = await Cart.find({ user: user._id, 'products.product': req.body.products[0].product });
+        let totalPrice = 0;
+
+        if (carts && carts.length > 0) {
+            carts.forEach(cart => {
+                cart.products.forEach(product => {
+                    if (product.product && req.body.products[0].product && product.product.toString() === req.body.products[0].product.toString()) {
+                        totalPrice += productData.price * req.body.products[0].quantity;
+                        product.quantity += req.body.products[0].quantity;
+                    }
+                });
+            });
+            
+
+            const updatedCart = await Cart.findByIdAndUpdate(carts[0]._id, { $set: carts[0], totalPrice: totalPrice, user: user.users._id }, { new: true });
+            res.status(200).json(updatedCart);
+        } else {
+            const newCart = await Cart.create({
+                user: user.users._id,
+                products: req.body.products,
+                totalPrice: productData.price * req.body.products[0].quantity,
+            });
+
+            const savedCart = await newCart.save();
+            res.status(200).json(savedCart);
+        }
     } catch (err) {
         res.status(500).json("Hata oluştu." + err);
     }
@@ -28,7 +43,7 @@ const updateCart = async (req, res) => {
 const deleteCart = async (req, res) => {
     try {
         await Cart.findByIdAndDelete(req.params.cartID);
-        res.status(200).json("Sepet başarıyla silindi.");
+        res.status(200).json("Ürün başarıyla sepetten çıkarıldı.");
     } catch (err) {
         res.status(500).json("Hata oluştu." + err);
     }
@@ -36,7 +51,7 @@ const deleteCart = async (req, res) => {
 
 const getCart = async (req, res) => {
     try {
-        const cart = await Cart.findOne({ user: req.params.userID });
+        const cart = await Cart.find({ user: req.params.userID }).populate('products.product');
         res.status(200).json(cart);
     } catch (err) {
         res.status(500).json("Hata oluştu." + err);
@@ -54,7 +69,6 @@ const getAllCarts = async (req, res) => {
 
 module.exports = {
     createCart,
-    updateCart,
     deleteCart,
     getCart,
     getAllCarts
